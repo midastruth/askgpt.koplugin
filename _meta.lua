1b5536d8fef8d29a9ffb07a6289ff377e0d13a0e
Gators King <pengtruth@gmail.com>
2025-11-01 21:55:17 +0800

    docs: update description to generalize AI query capability

--- _meta.lua
+++ _meta.lua
@@ -2,6 +2,6 @@ local _ = require("gettext")
 return {
     name = "askgpt",
     fullname = _("AskGPT"),
-    description = _([[Allows the user to query the ChatGPT API for answers to questions about highlighted text.]]),
+    description = _([[Allows the user to query AI for answers to questions about highlighted text.]]),
     version = 1.01,
 }
